7e778990ba5de0b791127d4cd9c6e989546a1957
Nolan McGill <nomcgill@gmail.com>
2019-08-04 08:17:36 -0500

    fixing macaddress udpate

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -3,9 +3,12 @@ import {shallow, mount} from 'enzyme';
 
 import Game from './game.js';
 
-
 describe('<Game />', () => {
     it('Renders without crashing', () =>{
         shallow(<Game />);
     })
+    // it('Renders the add button initially', () => {
+    //     const wrapper = shallow(<Game />);
+    //     expect(wrapper.hasClass('add-button')).toEqual(true);
+    // });
 })
