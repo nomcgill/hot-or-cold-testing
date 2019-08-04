269e94bdb2611234391aaeaa35bfcecbfd440d69
Nolan McGill <nomcgill@gmail.com>
2019-08-04 08:04:58 -0500

    first commit

---
+++ src/components/game.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import Game from './game.js';
+
+
+describe('<Game />', () => {
+    it('Renders without crashing', () =>{
+        shallow(<Game />);
+    })
+})
